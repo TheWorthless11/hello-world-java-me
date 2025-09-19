c0856f172b8b97dc7ee50e536613ff42a4478c8b
mahhia <mahhiamim@gmail.com>
2025-09-19 22:17:07 +0600

    add grade class

---
+++ mycode/grade.java
@@ -0,0 +1,13 @@
+public class grade {
+    public static String letter(int score){
+        if (score>=80) 
+        return "A";
+        if (score>= 70) 
+        return "B";
+        if(score>=60) 
+        return "C";
+        if(score>=50)
+        return "D";
+        return "F";
+    }
+}
